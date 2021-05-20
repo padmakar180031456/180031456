3217220702e81f362e2fdc1c6ef63a61a11990d7
PANDU <PANDU@LAPTOP-E4MK49F0.lan>
2021-05-20 12:22:12 +0530

    arrays

---
+++ arrays/src/arrays/Three.java
@@ -0,0 +1,18 @@
+package arrays;
+
+public class Three {
+	public static void main(String args[]) {
+		int a[]= {24,40,48,50,64,80};
+		int k=50,f=0,i;
+		for(i=0;i<a.length;i++)
+		{
+			if(a[i]==k) {
+				f=1;
+				break;
+			}
+		}
+		if(f==0)System.out.println(-1);
+		else System.out.println(i);
+	}
+
+}
