a27b857764a9750ffd68e578463200ad7eb81eb1
PANDU <PANDU@LAPTOP-E4MK49F0.lan>
2021-05-20 12:29:16 +0530

    strings

---
+++ strings/src/strings/Four.java
@@ -0,0 +1,12 @@
+package strings;
+import java.util.*;
+public class Four {
+	public static void main(String[] args)
+	 {
+		Scanner sc=new Scanner(System.in);
+	    String str = sc.nextLine();    
+	    System.out.println(str.substring(0, str.length()/2));
+	    sc.close();
+	  } 
+
+}
