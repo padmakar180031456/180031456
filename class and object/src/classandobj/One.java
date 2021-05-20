85d8cf308932384f2560e5ec838769f5ead9e8ba
PANDU <PANDU@LAPTOP-E4MK49F0.lan>
2021-05-20 12:23:42 +0530

    class and objects

---
+++ class and object/src/classandobj/One.java
@@ -0,0 +1,25 @@
+package classandobj;
+
+public class One {
+	double h,w,d;
+
+    one(double width,double height,double depth)
+   {
+       h=height;
+       w=width;
+       d=depth;
+   }
+   double volume()
+   { double v;
+   v=h*w*d;
+   return v;
+   }
+  
+   public static void main(String[] args) {
+      
+   one bc = new one(8.5,80.3,9.6);
+   System.out.println(bc.volume());
+
+   }
+
+}
